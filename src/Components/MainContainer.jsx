be15223141684b4257f027ec4dea3ffa2a1ddc88
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-29 23:38:05 +0530

    vedio container component created.

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ButtonList from "./ButtonList";
+import Vediocontainer from "./Vediocontainer";
 
 const MainContainer = () => {
   return (
     <div>
       <ButtonList />
+      <Vediocontainer />
     </div>
   );
 };
---
+++ src/Components/Vediocontainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Vediocontainer = () => {
+  return (
+    <div>
+      Vedio Container
+    </div>
+  )
+}
+
+export default Vediocontainer
