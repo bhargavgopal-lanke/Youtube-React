f5fc4be2c0ffe564ef0ddb216a8f2ac8468cb5d5
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-29 23:25:55 +0530

    Body component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import Layout from "./Components/Layout";
+import Body from "./Components/Body";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            {/* <Route index  /> */}
+            <Route index element={<Body />} />
           </Route>
         </Routes>
       </BrowserRouter>
