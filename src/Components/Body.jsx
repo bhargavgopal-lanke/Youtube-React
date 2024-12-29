b65c19a9538329af2ebc840ea738ca08329701b8
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-29 23:30:07 +0530

    main container and side bar components created.

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import SideBar from "./Sidebar.jsx";
+import MainContainer from "./MainContainer.jsx";
 
 const Body = () => {
   return (
     <div>
-      Body
+      <SideBar />
+      <MainContainer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
