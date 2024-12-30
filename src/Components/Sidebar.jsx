2b9b15d308ecf8839eefb90577e6d5da58402e79
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-30 16:42:59 +0530

    side nav UI added.

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div className="p-5 shadow-lg">
-      <h1>Subscriptions</h1>
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Vedios</li>
+        <li>Live</li>
+      </ul>
+      <h1 className="font-bold py-4">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      <h1 className="font-bold py-4">Watch Later</h1>
       <ul>
         <li>Music</li>
         <li>Sports</li>
