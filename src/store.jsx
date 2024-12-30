2ec626bbc3f3aaac5ea13b539911c0d93df72972
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-30 16:59:06 +0530

    slice imported in the store.

--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import headerSlice from "./Slices/Header-Slice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    headerInfo: headerSlice,
+  },
 });
 
 export default store;
