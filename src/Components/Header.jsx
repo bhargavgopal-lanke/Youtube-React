c491770535a78758edd3983810001a5f1676ad97
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-30 16:14:07 +0530

    code cleanup.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,10 +7,10 @@ import youtubelogo from "../Images/youtubelogo.png";
 const Header = () => {
   return (
     <div className="youtube-header-sec">
-      <div className="flex py-2 px-4 items-center">
+      <div className="flex py-2 px-4 items-center shadow-lg mb-3">
         <div className="w-2/12">
           <div className="flex items-center gap-5">
-            <GiHamburgerMenu className="text-2xl text-white " />
+            <GiHamburgerMenu className="text-2xl  " />
             <img alt="youtube logo" src={youtubelogo} className="w-3/6" />
           </div>
         </div>
@@ -23,7 +23,7 @@ const Header = () => {
                 className="block w-full p-4 pe-10 text-sm
                text-gray-900 border border-gray-300 rounded-full
                 bg-gray-50 focus:ring-blue-500 focus:border-blue-500
-                 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search"
                 required
               />
@@ -47,7 +47,7 @@ const Header = () => {
             </div>
           </div>
           <div className="rounded-full px-3 py-3 bg-gray-500">
-            <BiMicrophone className="text-white" />
+            <BiMicrophone className="" />
           </div>
         </div>
 
@@ -55,12 +55,12 @@ const Header = () => {
           <div className="flex justify-around items-center">
             <button
               type="button"
-              className="text-white w-4/12 bg-gray-500 content-center items-center rounded-full p-3"
+              className=" w-4/12 bg-gray-500 content-center items-center rounded-full p-3"
             >
               <PiPlusBold className="text-xl inline-block mr-2" /> Create
             </button>
-            <BiBell className="text-white text-3xl" />
-            <BiUserCircle className="text-white text-3xl" />
+            <BiBell className=" text-3xl" />
+            <BiUserCircle className=" text-3xl" />
           </div>
         </div>
       </div>
