df777e824f224faacbe49397bb21215ed86f1345
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-31 00:34:47 +0530

    fetching the data from youtube API and displaying the data.

--- src/Components/Vediocontainer.jsx
+++ src/Components/Vediocontainer.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VEDIOS_URL } from "../utils/Constants";
+import Vedios from "./Vedios";
 
 const Vediocontainer = () => {
+  const [vedios, setVedios] = useState([]);
+
+  const fetchVediosList = async () => {
+    const vedioList = await fetch(YOUTUBE_VEDIOS_URL);
+    const vedioListData = await vedioList.json();
+    setVedios(vedioListData.items);
+  };
+
+  useEffect(() => {
+    fetchVediosList();
+  }, []);
+
+  
+
   return (
     <div className="vedioContainer-sec">
-    
+      <Vedios vedios={vedios} />
     </div>
   );
 };
