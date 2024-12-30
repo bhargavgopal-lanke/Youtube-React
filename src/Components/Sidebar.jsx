f87d22317e642d823ce6d22397230329f07c1e6f
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-30 18:57:33 +0530

    instead of ternory or conditions we can use early return pattern.

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,33 +3,36 @@ import { useSelector } from "react-redux";
 
 const Sidebar = () => {
   const state = useSelector((store) => store?.appInfo?.toggle);
-  console.log("state", state);
+
+  // early return pattern
+  if (!state) return null;
+
   return (
     <>
-      {state === true && (
-        <div className="p-5 shadow-lg">
-          <ul>
-            <li>Home</li>
-            <li>Shorts</li>
-            <li>Vedios</li>
-            <li>Live</li>
-          </ul>
-          <h1 className="font-bold py-4">Subscriptions</h1>
-          <ul>
-            <li>Music</li>
-            <li>Sports</li>
-            <li>Gaming</li>
-            <li>Movies</li>
-          </ul>
-          <h1 className="font-bold py-4">Watch Later</h1>
-          <ul>
-            <li>Music</li>
-            <li>Sports</li>
-            <li>Gaming</li>
-            <li>Movies</li>
-          </ul>
-        </div>
-      )}
+      {/* {state === true && ( */}
+      <div className="p-5 shadow-lg">
+        <ul>
+          <li>Home</li>
+          <li>Shorts</li>
+          <li>Vedios</li>
+          <li>Live</li>
+        </ul>
+        <h1 className="font-bold py-4">Subscriptions</h1>
+        <ul>
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+          <li>Movies</li>
+        </ul>
+        <h1 className="font-bold py-4">Watch Later</h1>
+        <ul>
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+          <li>Movies</li>
+        </ul>
+      </div>
+      {/* )} */}
     </>
   );
 };
