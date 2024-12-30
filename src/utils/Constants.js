3c7b51d5940e90b7ed416b895c2f50e26b743043
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-31 00:33:24 +0530

    Api key updated.

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyAbM1e0UVhztSwj6EKCneB6SNe7xLh7qws";
+const GOOGLE_API_KEY = "AIzaSyCJixZ1wbG-uyuDNhdW8MIr_840w7yyGKs";
 
 export const YOUTUBE_VEDIOS_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
