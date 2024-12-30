487f7893372c065ef266a668740d076ef317af91
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-30 15:07:50 +0530

    develop branch commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         </div>
         <div className="w-7/12 flex items-center gap-5 justify-center my-auto">
           <div className="w-10/12 my-auto">
-            <div className="relative">
+            <div className="relative"> 
               <input
                 type="search"
                 id="default-search"
