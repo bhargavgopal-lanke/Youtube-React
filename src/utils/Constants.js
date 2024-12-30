9267e46cfcd0fccc33972a04ad528dfc80634d98
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-30 22:25:01 +0530

    youtube api added in the constant folder.

---
+++ src/utils/Constants.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyAbM1e0UVhztSwj6EKCneB6SNe7xLh7qws";
+
+export const YOUTUBE_VEDIOS_URL =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  GOOGLE_API_KEY;
