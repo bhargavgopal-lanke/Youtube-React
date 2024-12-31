1155d5a5d119a8e05d569b216ed49f699a04fada
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-31 16:04:24 +0530

    changed the API region to India.

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,5 @@
 const GOOGLE_API_KEY = "AIzaSyCJixZ1wbG-uyuDNhdW8MIr_840w7yyGKs";
 
 export const YOUTUBE_VEDIOS_URL =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=50&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
   GOOGLE_API_KEY;
