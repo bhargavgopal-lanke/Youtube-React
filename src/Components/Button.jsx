f5aaef556c7a6b9b1cc431fd450c319fab11cc27
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-01-02 17:10:28 +0530

    key warning  issue fix.

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,11 +3,12 @@ import React from "react";
 const Button = ({ name }) => {
   return (
     <div>
-      {name.map((val) => {
+      {name.map((val, index) => {
         return (
           <button
             type="button"
             className="px-5 py-2 m-2 bg-gray-100 rounded-lg"
+            key={index}
           >
             {val}
           </button>
