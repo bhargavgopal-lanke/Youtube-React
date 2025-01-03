8fc2851ab8406410bfbcc51347b09beea386655c
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-01-03 14:39:44 +0530

    adding a higher order component.

--- src/Components/Vediocontainer.jsx
+++ src/Components/Vediocontainer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { YOUTUBE_VEDIOS_URL } from "../utils/Constants";
-import VedioCard from "./VedioCard";
+import VedioCard, { AdVedioCard } from "./VedioCard";
 import { Link } from "react-router";
 
 const Vediocontainer = () => {
@@ -18,10 +18,11 @@ const Vediocontainer = () => {
 
   return (
     <div className="vedioContainer-sec flex flex-wrap">
+      {vedios[0] && <AdVedioCard val={vedios[0]} />}
       {vedios.map((val) => {
         return (
           <Link to={"/watch?v=" + val.id} key={val.id}>
-            <VedioCard  val={val} />
+            <VedioCard val={val} />
           </Link>
         );
       })}
