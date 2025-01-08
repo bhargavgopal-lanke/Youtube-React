5c6bb986829079883eba2bd882c56732e58f254f
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-01-08 23:58:19 +0530

    slice created to store search data in the store.

--- src/store.jsx
+++ src/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./Slices/appSlice";
+import searchSlice from "./Slices/SearchDataSlice";
 
 const store = configureStore({
   reducer: {
     appInfo: appSlice,
+    searchInfo: searchSlice,
   },
 });
 
