12d946dc9c385fe85ad9eb5afc4fa3455f39083e
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-01-25 17:05:06 +0530

    UI issue fix.

--- src/Components/LiveChat/ChatMessage.jsx
+++ src/Components/LiveChat/ChatMessage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { IMAGEPATH } from "../../utils/Constants";
 
-const ChatMessage = ({ image, reply }) => {
+const ChatMessage = ({ name, reply }) => {
   return (
     <div className="flex items-center">
-      <img src={image} alt="chat-img" className="rounded-full w-10" />
-      <p className="pl-2">{reply}</p>
+      <img src={IMAGEPATH} alt="user-img" className="rounded-full w-10" />
+      <p className="px-2 font-bold">{name}</p>
+      <p>{reply}</p>
     </div>
   );
 };
--- src/Components/LiveChat/LiveChat.jsx
+++ src/Components/LiveChat/LiveChat.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ChatMessage from "./ChatMessage";
-import { IMAGEPATH } from "../../utils/Constants";
 
 const LiveChat = () => {
   return (
     <div className="w-full h-[600px] m-2 p-2 border border-black bg-slate-100 rounded-lg">
-      <ChatMessage image={IMAGEPATH} reply={"Chat reply"} />
+      <ChatMessage name={"Bhargav"}  reply={"Chat reply"} />
     </div>
   );
 };
