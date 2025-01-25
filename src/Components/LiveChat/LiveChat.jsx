2b3fe28972908ba742872fca12b10edcd8b774fd
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-01-25 18:56:53 +0530

    Live chat working with static data.

--- src/Components/LiveChat/LiveChat.jsx
+++ src/Components/LiveChat/LiveChat.jsx
@@ -1,11 +1,20 @@
 import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { setLiveChat } from "../../Slices/LiveChatSlice";
 
 const LiveChat = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
     const i = setInterval(() => {
       // Api Polling
       console.log("Api polling");
+      dispatch(
+        setLiveChat({
+          name: "Bhargav",
+          reply: "Api Polling",
+        })
+      );
     }, 2000);
 
     return () => {
@@ -13,13 +22,14 @@ const LiveChat = () => {
     };
   }, []);
 
+  const { messages } = useSelector((state) => state?.LiveChatInfo);
+
   return (
-    <div className="w-full h-[600px] m-2 p-2 border border-black bg-slate-100 rounded-lg">
-      <ChatMessage name={"Bhargav"} reply={"Chat reply"} />
-      <ChatMessage name={"Bhargav"} reply={"Chat reply"} />
-      <ChatMessage name={"Bhargav"} reply={"Chat reply"} />
-      <ChatMessage name={"Bhargav"} reply={"Chat reply"} />
-      <ChatMessage name={"Bhargav"} reply={"Chat reply"} />
+    <div className="w-full h-[600px] m-2 p-2 border
+     border-black bg-slate-100 rounded-lg overflow-y-scroll">
+      {messages.map((msgs) => {
+        return <ChatMessage name={msgs.name} reply={msgs.reply} />;
+      })}
     </div>
   );
 };
