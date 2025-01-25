86693b5ab8952228182dbb92562ccd7d126e894d
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-01-25 20:37:52 +0530

    code format

--- src/Components/LiveChat/ChatMessage.jsx
+++ src/Components/LiveChat/ChatMessage.jsx
@@ -3,11 +3,11 @@ import { IMAGEPATH } from "../../utils/Constants";
 
 const ChatMessage = ({ name, reply }) => {
   return (
-    <div className="flex flex-wrap items-center shadow-sm p-2">
-      <img src={IMAGEPATH} alt="user-img" className="rounded-full w-10" />
-      <span className="px-2 font-bold">{name}</span>
-      <span>{reply}</span>
-    </div>
+      <div className="flex flex-wrap items-center shadow-sm p-2">
+        <img src={IMAGEPATH} alt="user-img" className="rounded-full w-10" />
+        <span className="px-2 font-bold">{name}</span>
+        <span>{reply}</span>
+      </div>
   );
 };
 
