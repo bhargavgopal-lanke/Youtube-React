c9581748a04419bc705a804b0d50bd6495686f3c
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-01-25 19:31:24 +0530

    created a function for generating random messages.

---
+++ src/utils/helper/RandomStrings.js
@@ -0,0 +1,13 @@
+export function getRandomStrings() {
+  let result = "";
+  const length = 30;
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
