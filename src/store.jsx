19cea4e0dc06a1aae424fbf932d9cf9f7028f39f
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-01-25 17:28:53 +0530

    live chat slice imprted in the store.

--- src/store.jsx
+++ src/store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./Slices/appSlice";
 import searchSlice from "./Slices/SearchDataSlice";
+import LiveChatReducer from "./Slices/LiveChatSlice";
 
 const store = configureStore({
   reducer: {
     appInfo: appSlice,
     searchInfo: searchSlice,
+    LiveChatInfo: LiveChatReducer,
   },
 });
 
